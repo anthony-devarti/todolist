a87cf68335e87e1a2686cea78a1e3339ac458c59
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-24 22:35:00 +0000

    got the deleted items to hide

--- src/ToDoitem.js
+++ src/ToDoitem.js
@@ -2,20 +2,10 @@
 // import { useState } from "react";
 
 export default function ToDoItem({setToDos, toDos}){
-    // const [checked, setChecked] = useState(false);
-    // const [deleted, setDeleted] = useState(false);
-    //for loop that generates one of these divs for each object in the toDos array
-    //something isn't right with the delete button.  "Warning: Received `false` for a non-boolean attribute `deleted`."
     
-    //make a function that maps over the entire array and fills stuff in.  The temp vars are doing the work now.
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
     //https://reactjs.org/docs/lists-and-keys.html
     console.log('I am rendering')
-    //I can track todo items from the list according to their index just fine.  Need to be able to track them by their key
-    // console.log(toDos[0])
-
-    //I need to print a different row with the text of the todo item visible.  
-    //The div that contains it should have its status and unique key, but they do not need to be visible
  
     function ListItem(toDo, uniqueId, status){
       console.log('todo', toDo, uniqueId, status)
@@ -53,7 +43,7 @@ export default function ToDoItem({setToDos, toDos}){
 
     //the x button needs to hand the id of the item to which it is attached over to the delete function
     function toDoList(toDos){
-      const toDoList = toDos.map((item)=>
+      const toDoList = toDos.filter(item=>(item.status!=='deleted')).map((item)=>
         <>
         <ListItem uniqueId={item.id} key={item.id} text={item.text} status='active'/>
         <button id={item.id} variant='outline-danger' onClick={(e)=>{
